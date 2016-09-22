2894c1eb2071a55f43889424b01870914a0a5d42
Michael Lefkowitz <lefkowitz.michael@gmail.com>
2016-09-22 18:28:15 -0500

    Added settings and asteroid obj

---
+++ scripts/asteroids.js
@@ -0,0 +1,13 @@
+var settings = {
+  canvasWidth: 1000,
+  canvasHeight: 700,
+  asteroidRadius: 20
+}
+
+var asteroids = [
+  {id: 'a1'},
+  {id: 'a2'},
+  {id: 'a3'},
+  {id: 'a4'},
+  {id: 'a5'}
+];
