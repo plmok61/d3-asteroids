25cf2041bb28303f8b05f2374dbb78947c80993f
Michael Lefkowitz <lefkowitz.michael@gmail.com>
2016-09-23 10:02:16 -0500

    Asteroids appearing. Transition causing errors

--- scripts/asteroids.js
+++ scripts/asteroids.js
@@ -16,34 +16,35 @@ var highScore = 0;
 var score = 0;
 var shield = 100;
 
-var canvas = d3.select('body')
+var canvas = d3.select('.boardcontainer')
   .append('svg')
   .attr('width', settings.canvasWidth)
   .attr('height', settings.canvasHeight)
   .attr('class', 'gameboard')
 
 var circles = canvas.selectAll('circle')
-  .data(dataArray)
+  .data(asteroids)
   .enter()
     .append('circle')
-      .attr('cx', function(d) { return Math.random() * canvasWidth})
-      .attr('cy', function(d) { return Math.random() * canvasHeight})
-      .attr('r', 20)
+      .attr('id', function(d) { return d.id})
+      .attr('cx', function(d) { return Math.random() * settings.canvasWidth})
+      .attr('cy', function(d) { return Math.random() * settings.canvasHeight})
+      .attr('r', settings.asteroidRadius)
       .attr('fill', 'red')
-      .transition()
-        .duration(function(d) { return Math.random() * 1000 + 1000})
-        .ease("cubic")
-        .attr("cx", Math.random() * canvasWidth)
-        .attr("cy", Math.random() * canvasHeight)
-        .each(slide);
-
-function slide() {
-  var circle = d3.select(this);
-  (function repeat() {
-    circle = circle.transition()
-        .duration(Math.random() * 1000 + 1000)
-        .attr("cx", Math.random() * canvasWidth)
-        .attr("cy", Math.random() * canvasHeight)
-        .each("end", repeat);
-  })();
-};
+      // .transition()
+      //   .duration(function(d) { return Math.random() * 1000 + 1000})
+      //   .ease("cubic")
+      //   .attr("cx", Math.random() * settings.canvasWidth)
+      //   .attr("cy", Math.random() * settings.canvasHeight)
+//         .each(slide);
+//
+// function slide() {
+//   var circle = d3.select(this);
+//   (function repeat() {
+//     circle = circle.transition()
+//         .duration(Math.random() * 1000 + 1000)
+//         .attr("cx", Math.random() * settings.canvasWidth)
+//         .attr("cy", Math.random() * settings.canvasHeight)
+//         .each("end", repeat);
+//   })();
+// };
