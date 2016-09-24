251ae55a9b00786db562b49df000765475825501
Phil <plmok61@gmail.com>
2016-09-24 14:02:52 -0500

    track move and score timer

--- scripts/asteroids.js
+++ scripts/asteroids.js
@@ -48,3 +48,51 @@ function slide() {
         .each("end", repeat);
   })();
 };
+
+var asteroids = canvas.selectAll('circle');
+var spaceShip = canvas.select('#player');
+
+var isAlreadyTouching = false;
+
+function trackMove() {
+  var playerX = spaceShip.node().x.animVal.value + 20;
+  var playerY = spaceShip.node().y.animVal.value + 20;
+
+  asteroids.each(function() {
+  var circle = d3.select(this);
+    var circleX = circle[0][0].cx.animVal.value;
+    var circleY = circle[0][0].cy.animVal.value;
+
+    if (isAlreadyTouching === false &&
+        Math.abs(playerX - circleX) < 40 &&
+        Math.abs(playerY - circleY) < 40 ) {
+      isAlreadyTouching = true;
+    console.log(isAlreadyTouching)
+      collisionCount--
+      d3.select('div.collisions span').text(collisionCount);
+    }
+    if (Math.abs(playerX - circleX) > 40 &&
+        Math.abs(playerY - circleY) > 40) {
+      isAlreadyTouching = false;
+    console.log(isAlreadyTouching)
+    }
+
+    if (collisionCount <= 0) {
+      if (score > highScore) {
+        highScore = score;
+        d3.select('div.highscore span').text(highScore)
+      }
+      score = 0;
+      collisionCount = 100;
+    }
+
+  })
+  requestAnimationFrame(trackMove)
+}
+
+var timer = setInterval(function() {
+  score++
+  d3.select('div.current span').text(score)
+},100)
+
+trackMove();
